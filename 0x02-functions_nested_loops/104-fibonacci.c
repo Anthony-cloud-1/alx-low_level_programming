ac22ac71153ba450a2be977896f3b8fea0ded6c8
Anthony-cloud-1 <tonysack13@gmail.com>
2023-08-15 10:30:14 -0700

    loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,31 @@
  */
 int main(void)
 {
-	unsigned long int fib1 = 1, fib2 = 2, next;
-	int count;
+	unsigned long int i; before = 1; after = 2; l = 1000000000; before1; before2; after1; after2;
 
-	printf("%lu, %lu", fib1, fib2);
-	
-	for (count = 3; count <= 98; count++)
+	printf("%lu", before);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = fib1 + fib2;
-		printf(", %lu", next);
-		fib1 = fib2;
-		fib2 = next;
+		printf(", %lu", after);
+		after += before;
+		before = after - before;
 	}
 
-	printf("\n");
+	before1 = (before / l);
+	before2 = (before % l);
+	after1 = (after / l);
+	after2 = (after % l);
 
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", after1 + (after2 / l));
+		printf("%lu", after2 % l);
+		after1 = after1 + before1;
+		before1 = after1 - before1;
+		after2 = after2 + before2;
+		before2 = after2 - before2;
+	}
+	printf("\n");
 	return (0);
 }
