e0e4524c3f700ae746eeadd92adf0d43c0169beb
Anthony-cloud-1 <tonysack13@gmail.com>
2023-08-17 11:38:24 -0700

    nest

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,18 +10,25 @@ void print_number(int n)
 {
 	int divisor = 1;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-
-	while (n / divisor >= 10)
+	 while (n / divisor)
 		divisor *= 10;
 
-	while (divisor != 0)
+	while (divisor > 1)
 	{
-		_putchar((n / divisor) % 10 + '0');
 		divisor /= 10;
+		_putchar((n / divisor) % 10 + '0');
 	}
+
+	_putchar(n % 10 + '0');
 }
