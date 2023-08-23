87f3f3ad16a51a14304ea1f3021336ae4ce9bf3a
Anthony-cloud-1 <tonysack13@gmail.com>
2023-08-23 15:06:49 -0700

    array

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @src: source string
+ * @dest: destination string
+ * Return: A pointer to the resulting dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	len = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
