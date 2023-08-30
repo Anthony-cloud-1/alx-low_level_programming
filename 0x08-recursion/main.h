bff2cd57f806cd3a60a579885a612a7a5866a328
Anthony-cloud-1 <tonysack13@gmail.com>
2023-08-30 16:06:04 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,8 @@ int _recursion(int n, int i);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int actual_prime(int n, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
 
